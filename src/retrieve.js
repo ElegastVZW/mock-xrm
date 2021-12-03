4c7481ab3b33885cd3aaf9bb14cc8ca2b7d7c9c7
TimVanOnckelen <TimVanOnckelen@EL-008601611157>
2021-12-03 13:18:45 +0100

    Fix non exsiting CollectionNames

--- src/retrieve.js
+++ src/retrieve.js
@@ -3,7 +3,14 @@ import retrieveMetadataWrapperMemo from './retrieveMetadataWrapperMemo';
 
 const retrieve = async (entityLogicalName, id, options = '') => {
     let meta = await retrieveMetadataWrapperMemo(entityLogicalName);
-    let result = await executeGet(`${meta.EntityMetadata.LogicalCollectionName}(${id})${options ? options : ''}`)();
+    let LogicalCollectionNameTemp;
+    // Fix when LogicalCollectionName is not set
+    if(meta.EntityMetadata.LogicalCollectionName){
+        LogicalCollectionNameTemp = meta.EntityMetadata.LogicalName+"s";
+    }else{
+        LogicalCollectionNameTemp = meta.EntityMetadata.LogicalCollectionName;
+    }
+    let result = await executeGet(`${LogicalCollectionNameTemp}(${id})${options ? options : ''}`)();
     return result.json();
 }
 
